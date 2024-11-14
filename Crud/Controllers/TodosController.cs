d0477f9ccf36fb8c4fe2b47f29c16398269b436b
tahahat4 <tahahat4@gmail.com>
2024-11-14 13:28:15 +0100

    'Final'

---
+++ Crud/Controllers/TodosController.cs
@@ -0,0 +1,95 @@
+﻿using Crud.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.Controllers
+{
+    public class TodosController : Controller
+    {
+
+        public static List<TodoVM> Todos = new List<TodoVM>();
+        private static int nextId = 1;
+
+
+        public IActionResult Index()
+        {
+            return View(Todos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(TodoVM todo)
+        {
+            if (ModelState.IsValid)
+            {
+                todo.Id = nextId++;
+                Todos.Add(todo);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var todo = Todos.FirstOrDefault(item => item.Id == id);
+            if (todo == null) return NotFound();
+
+            return View(todo);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var todo = Todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+            return View(todo);
+        }
+
+        // POST: Delete a specific todo
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var todo = Todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+
+            Todos.Remove(todo);
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: Display form to edit an existing todo
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var todo = Todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TodoVM updatedTodo)
+        {
+            if (ModelState.IsValid)
+            {
+                var todo = Todos.FirstOrDefault(t => t.Id == updatedTodo.Id);
+                if (todo == null) return NotFound();
+
+                todo.Title = updatedTodo.Title;
+                todo.Description = updatedTodo.Description;
+                todo.IsFinished = updatedTodo.IsFinished;
+                todo.SelectedTags = updatedTodo.SelectedTags;
+                todo.User = updatedTodo.User;
+                todo.CreateAd = updatedTodo.CreateAd;
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Crud/ViewModel/TodoVM.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud.ViewModel
+{
+
+    public enum TodoTag
+    {
+        Bug ,
+        Test ,
+        Task
+    } 
+    public class TodoVM
+    {
+
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "darouri title")]
+        [RegularExpression("[A-Z][A-Za-z0-9]{1,50}", ErrorMessage = "Title machi nichane")]
+        public string Title { get; set; } 
+
+
+        public string Description { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public List<TodoTag> SelectedTags { get; set; } = new List<TodoTag>();
+
+        [Required(ErrorMessage ="darouri tkhtar user")]
+        public string User { get; set; }
+
+        [Required(ErrorMessage ="darouri dakhal datetime")]
+        [DataType(DataType.DateTime)]
+        public DateTime CreateAd { get; set; }
+
+
+
+    }
+}
